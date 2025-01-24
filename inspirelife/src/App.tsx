9000476c0705be02c1e1a98d2346fb5104abf9d3
Excaliuz <satyamchaturvedi39@gmail.com>
2025-01-24 18:29:37 +0530

    Policies Page Created,cleared merge conflicts and basic routing

--- inspirelife/src/App.tsx
+++ inspirelife/src/App.tsx
@@ -1,17 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Component Imports
 import Header from "./components/Header";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 
-function App() {
+// Page Imports
+import Policies from "./pages/Policies";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <Header />
+    <Router>
+      <Header /> {/* Optional: Consider adding if you had it in your imports */}
+      
       <Navbar />
-      <Hero />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/policies" element={<Policies />} />
+        <Route path="*" element={<Hero />} /> {/* Redirects to Home for unknown routes */}
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
-}
+};
 
 export default App;
