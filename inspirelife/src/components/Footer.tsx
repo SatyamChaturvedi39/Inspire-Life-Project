1b3f62fa91d8007c59db507de94015c8c8861620
TrishaRaj0410 <trishatr04@gmail.com>
2025-01-24 23:37:38 +0530

    Footer Update

--- inspirelife/src/components/Footer.tsx
+++ inspirelife/src/components/Footer.tsx
@@ -12,12 +12,14 @@ const Footer: React.FC = () => {
             alt="Inspire Life Logo"
             className="footer-logo"
           />
-          <h3>Inspire Life Insurance Solutions</h3>
+          <div className="footer-Company">
+          <h5>Inspire Life Insurance Solutions</h5>
           <p>Jayanagar, Bengaluru</p>
+          </div>
         </div>
         <div className="footer-columns">
           <div className="footer-column">
-            <h4>Company</h4>
+            <h4><b>Company</b></h4>
             <ul>
               <li>About us</li>
               <li>Policies</li>
@@ -26,20 +28,34 @@ const Footer: React.FC = () => {
             </ul>
           </div>
           <div className="footer-column">
-            <h4>Services</h4>
+            <h4><b>Services</b></h4>
             <ul>
               <li>Renewals</li>
               <li>Self Service</li>
             </ul>
           </div>
           <div className="footer-column">
-            <h4>Group Companies</h4>
+            <h4><b>Group Companies</b></h4>
             <ul>
               <li>Lic</li>
               <li>Star Health</li>
               <li>Care Health</li>
             </ul>
           </div>
+        </div>
+        <div className="sitemap">
+          <h4>Sitemap</h4>
+          <ul>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/services">Services</a></li>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/careers">Careers</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </div>
+        <hr></hr>
+        <div className="footer-CR">
+        <p>&#169; {new Date().getFullYear()}. All Rights Reserved.</p>
         </div>
       </div>
     </footer>
