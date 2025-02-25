2fa09fcdf5536dd219a7b2a3795777ba845d7840
Excaliuz <satyamchaturvedi39@gmail.com>
2025-02-25 19:13:28 +0530

    meow

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -44,7 +44,6 @@ const NavBar = () => {
             Contact
           </Link>
         </li>
-<<<<<<< Updated upstream
         {/* Login Page Link */}
         <li className="login-link">
           <Link
@@ -52,15 +51,6 @@ const NavBar = () => {
             className={location.pathname.startsWith("/login") ? "active" : ""}
           >
             Login
-=======
-        {/* Admin Dashboard Link */}
-        <li className="admin-link">
-          <Link
-            to="/admin"
-            className={location.pathname.startsWith("/admin") ? "active" : ""}
-          >
-            Admin
->>>>>>> Stashed changes
           </Link>
         </li>
       </ul>
